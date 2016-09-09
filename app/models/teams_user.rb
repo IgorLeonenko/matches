da356bb4b424845199dafffaec5d75bf5eb38930
Igor Leonenko <IgorLeon@montemac-a008.b1.monterail.intra>
2016-09-09 11:02:58 +0200

    add validation for same player in one team

--- app/models/teams_user.rb
+++ app/models/teams_user.rb
@@ -1,4 +1,6 @@
 class TeamsUser < ApplicationRecord
   belongs_to :team
   belongs_to :user
+
+  validates :user_id, uniqueness: { scope: :team_id, message: 'already in team' }
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,13 +3,19 @@ require 'rails_helper'
 RSpec.describe Team, type: :model do
 
   describe '.validate' do
-    let(:first_team) { create(:team) }
+    let(:first_team)  { create(:team) }
     let(:second_team) { create(:team) }
+    let(:user)        { create(:user) }
 
     context 'when invalid data' do
       it 'with same team name' do
         first_team.name = second_team.name
         expect(first_team).not_to be_valid
+      end
+
+      it 'try to add same player to team' do
+        first_team.users << user
+        expect{first_team.users << user}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: User already in team')
       end
     end
 
