76db02feac54e808cc9eebc966f07f5c2283239d
Igor Leonenko <IgorLeon@montemac-a008.b1.monterail.intra>
2016-09-12 09:58:31 +0200

    [fix]change name of validate method

--- app/models/match.rb
+++ app/models/match.rb
@@ -9,13 +9,15 @@ class Match < ApplicationRecord
   validates :home_team_score, presence: true, numericality: true
   validates :invited_team_score, presence: true, numericality: true
   validates :status, inclusion: { in: STATUS }, allow_nil: true
-  validate  :players_in_team
+  validate  :player_can_be_only_in_one_team_on_match
 
-  def players_in_team
-    if home_team.present? && invited_team.present? && !home_team.users.empty? && !invited_team.users.empty?
-      if home_team.users == invited_team.users
-        errors.add(:base, "Same player in different teams")
+  private
+
+    def player_can_be_only_in_one_team_on_match
+      if home_team.present? && invited_team.present? && !home_team.users.empty? && !invited_team.users.empty?
+        if home_team.users == invited_team.users
+          errors.add(:base, "Same player in different teams")
+        end
       end
     end
-  end
 end
