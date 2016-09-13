d978973accd48d0f5c9433955721c5a9131fe544
Igor Leonenko <igrleon@gmail.com>
2016-09-13 15:04:16 +0200

    [fix] fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ team_names  = %w(winners assassins bears eagles)
 game_names  = %w(fighting nascar wargame pool)
 users       = []
 teams       = []
+games       = []
 
 4.times do |i|
   users << User.create(name: "#{names[i-1]}", email: "example-#{user_names[i-1]}@mail.com",
@@ -24,16 +25,16 @@ teams[0].users << users[0]
 teams[0].users << users[1]
 teams[1].users << users[2]
 teams[1].users << users[3]
-teams[2].users << users[3]
 teams[2].users << users[1]
+teams[2].users << users[0]
 teams[3].users << users[2]
-teams[3].users << users[0]
-
-Match.create(name: 'fighting', home_team: teams[0], invited_team: teams[1])
-Match.create(name: 'football', home_team: teams[2], invited_team: teams[1])
-Match.create(name: 'billiard', home_team: teams[3], invited_team: teams[0])
-Match.create(name: 'guitar_battle', home_team: teams[2], invited_team: teams[3])
+teams[3].users << users[3]
 
 4.times do |i|
-  Game.create(name: "#{game_names[i-1]}")
+  games << Game.create(name: "#{game_names[i-1]}")
 end
+
+Match.create(name: 'fighting', home_team: teams[0], invited_team: teams[1], game: games[0])
+Match.create(name: 'football', home_team: teams[1], invited_team: teams[2], game: games[1])
+Match.create(name: 'billiard', home_team: teams[3], invited_team: teams[0], game: games[2])
+Match.create(name: 'guitar_battle', home_team: teams[2], invited_team: teams[3], game: games[3])
