e249b70be3d9b8a299b08a9437cefd19558ff6be
Igor Leonenko <igrleon@gmail.com>
2016-09-14 10:26:52 +0200

    add rescue if record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  private
+
+    def render_404
+      render :file => "public/404.html", :status => 404, :layout => false
+    end
 end
