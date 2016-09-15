43509ae6282164bd704c6ab37b3ccf28741c74f7
Igor Leonenko <igrleon@gmail.com>
2016-09-15 12:52:10 +0200

    add match controller tests

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe MatchesController, type: :controller do
-  let(:test_match) { create(:match, home_team: create(:team_with_users), invited_team: create(:team_with_users)) }
-  # let(:new_match) { build(:match, home_team: test_match.home_team, invited_team: test_match.invited_team) }
+  let(:team_1) { create(:team_with_users) }
+  let(:team_2) { create(:team_with_users) }
+  let(:test_match) { create(:match, home_team: team_1, invited_team: team_2 ) }
 
   describe 'GET #index' do
     before { get :index }
@@ -33,12 +34,126 @@ RSpec.describe MatchesController, type: :controller do
   end
 
   describe 'POST #create' do
-
     context 'with valid attributes' do
+      subject { post :create, params: { match: attributes_for(:match,
+                                               home_team_id: team_1.id,
+                                               invited_team_id: team_2.id ) } }
+
       it 'create a new match' do
-        expect{ post :create, match: test_match.attributes }.to change(Match, :count).to(1)
+        expect{ subject }.to change(Match, :count).by(1)
       end
 
+      it 'redirects to @match' do
+        expect(subject).to redirect_to Match.last
+        expect(subject.response_code).to eq(302)
+      end
+
+      it 'has flash[:notice] message' do
+        subject
+        expect(flash[:notice]).to be_present
+        expect(flash[:notice]).to include('Match created sucessfully')
+      end
+    end
+
+    context 'with invalid attributes' do
+      subject { post :create, params: { match: attributes_for(:match,
+                                               home_team_id: 'bad_id',
+                                               invited_team_id: 'bad_id' ) } }
+      it 'not create a new match' do
+        expect{ subject }.to_not change(Match, :count)
+      end
+
+      it 're-render action new' do
+        expect(subject).to render_template(:new)
+        expect(subject.response_code).to eq(200)
+      end
+
+      it 'has flash[:alert] message' do
+        subject
+        expect(flash[:alert]).to be_present
+        expect(flash[:alert]).to include('Something wrong')
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'with valid attributes' do
+      subject { put :update, params: { id: test_match, match: attributes_for(:match,
+                                                       home_team_id: team_1.id,
+                                                       invited_team_id: team_2.id ) } }
+
+      it 'located the requested @match' do
+        subject
+        expect(assigns(:match)).to eq(test_match)
+      end
+
+      it 'change @match attributes' do
+        put :update, params: { id: test_match, match: attributes_for(:match,
+                                                      name: 'TestMatch',
+                                                      home_team_id: team_1.id,
+                                                      invited_team_id: team_2.id ) }
+        test_match.reload
+        expect(test_match.name).to eq('TestMatch')
+      end
+
+      it 'redirects to updated @match' do
+        expect(subject).to redirect_to(test_match)
+        expect(subject.response_code).to eq(302)
+      end
+
+      it 'has flash[:notice] message' do
+        subject
+        expect(flash[:notice]).to be_present
+        expect(flash[:notice]).to include('Match edited sucessfully')
+      end
+    end
+
+    context 'with invalid attributes' do
+      subject do
+        put :update, params: { id: test_match, match: attributes_for(:match,
+                                                       name: nil,
+                                                       home_team_id: nil,
+                                                       invited_team_id: team_2.id ) }
+        test_match.reload
+      end
+
+      it 'does not change @match attributes' do
+        subject
+        expect(test_match.name).to eq(test_match.name)
+      end
+
+      it 're-render action edit' do
+        expect(subject).to render_template(:edit)
+        expect(response.code).to eq('200')
+      end
+
+      it 'has flash[:alert] message' do
+        subject
+        expect(flash[:alert]).to be_present
+        expect(flash[:alert]).to include('Something went wrong ')
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      before  { test_match }
+
+      context 'with valid attributes' do
+        subject { delete :destroy, params: { id: test_match.id } }
+
+        it 'deletes the @match' do
+          expect{ subject }.to change(Match, :count).by(-1)
+        end
+
+        it 'redirects to matches#index' do
+          expect(subject).to redirect_to(matches_path)
+        end
+
+        it 'has flash[:notice] message' do
+          subject
+          expect(flash[:notice]).to be_present
+          expect(flash[:notice]).to include('Match deleted sucessfully')
+        end
+      end
     end
   end
 end
--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :match do
     name   { Faker::Lorem.characters(10) }
-    game   { create(:game) }
+    game_id   { create(:game).id }
     status 'prepare'
     home_team_score 0
     invited_team_score 0
