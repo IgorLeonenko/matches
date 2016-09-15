40ff1df2c4dfc4bf5efde2addad46ce8a49c5b56
Igor Leonenko <igrleon@gmail.com>
2016-09-15 14:49:31 +0200

    add users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'matches#index'
+  root 'users#new'
+
+  get 'sign_up' => 'users#new', as: 'sign_up'
 
   resources :matches
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
