7519964981e0927a66d9a85cd33a56a04d093a93
Igor Leonenko <igrleon@gmail.com>
2016-09-15 16:40:30 +0200

    [fix] change routes links

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   resources :matches
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
