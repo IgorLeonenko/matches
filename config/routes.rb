542d3bc3d7a975fdce32f4618b63c89ac3b8fe3e
Igor Leonenko <igrleon@gmail.com>
2016-09-21 12:55:36 +0200

    [fix] remove unnecessary brackets

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   resources :matches
 
-  resources :users, only: [:create]
+  resources :users, only: :create
   get 'sign_up' => 'users#new', as: 'sign_up'
 
-  resources :sessions, only: [:create]
+  resources :sessions, only: :create
   get 'log_in'  => 'sessions#new', as: 'log_in'
   delete 'log_out' => 'sessions#destroy', as: 'log_out'
 
