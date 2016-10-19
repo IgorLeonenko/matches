de547bce10b959217434310ae6abd828c85c6842
Igor Leonenko <igrleon@gmail.com>
2016-10-19 17:12:16 +0200

    set token automatically to headers

--- front-client/src/api.js
+++ front-client/src/api.js
@@ -4,4 +4,13 @@ const API_PATH = 'http://localhost:3000/api/v1'
 
 axios.defaults.baseURL = API_PATH
 
-export const getMatches = () => axios.get('/matches', {headers: {'Authorization': 'Bearer ' + localStorage.getItem('id_token')}})
+export const getMatches = () => axios.get('/matches')
+
+export default {
+  logIn () {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token')
+  },
+  logOut () {
+    axios.defaults.headers.common['Authorization'] = ''
+  }
+}
