3f4e89e793940b6d025c142f4544f845a5563086
Igor Leonenko <igrleon@gmail.com>
2016-10-20 13:11:49 +0200

    add response interceptor

--- front-client/src/api.js
+++ front-client/src/api.js
@@ -1,10 +1,21 @@
 import axios from 'axios'
+import auth from './auth'
+import router from './main'
 
 const API_PATH = 'http://localhost:3000/api/v1'
 
 axios.defaults.baseURL = API_PATH
 
 export const getMatches = () => axios.get('/matches')
+
+axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  if (error.response.status === 401 || error.response.status === 403) {
+    auth.logout()
+    router.push('/login')
+  }
+})
 
 export default {
   logIn () {
