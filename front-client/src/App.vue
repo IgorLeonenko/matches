825c2010cfe7e15d705ea486d93e604cf1b8e135
Igor Leonenko <igrleon@gmail.com>
2016-10-21 17:43:42 +0200

    add new logo

--- front-client/src/App.vue
+++ front-client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/monterail-logo.png">
     <navigation></navigation>
     <router-view></router-view>
   </div>
