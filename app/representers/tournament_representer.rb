7b6daec273ce73cf343ef3bfde427fa3aec18ec7
Igor Leonenko <igrleon@gmail.com>
2016-10-27 15:24:06 +0200

    change tournaments representers

--- app/representers/tournament_representer.rb
+++ app/representers/tournament_representer.rb
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 class TournamentRepresenter < BaseRepresenter
-=======
-class TournamentRepresenter
->>>>>>> add representer for round & change representer for tournament
   def initialize(tournament)
     @tournament = tournament
   end
 
-<<<<<<< HEAD
   def basic
     {
       id: @tournament.id,
@@ -37,21 +32,5 @@ class TournamentRepresenter
     with_teams_and_users.merge(
       rounds: RoundsRepresenter.new(@tournament.rounds).with_matches,
     )
-=======
-  def as_json(_ = {})
-    {
-        id: @tournament.id,
-        title: @tournament.title,
-        description: @tournament.description,
-        style: @tournament.style,
-        state: @tournament.state,
-        start_date: @tournament.start_date,
-        teams_quantity: @tournament.teams_quantity,
-        teams_size: @tournament.teams.size,
-        players_in_team: @tournament.players_in_team,
-        teams: TeamsRepresenter.new(@tournament.teams),
-        rounds: RoundsRepresenter.new(@tournament.rounds)
-      }
->>>>>>> add representer for round & change representer for tournament
   end
 end
