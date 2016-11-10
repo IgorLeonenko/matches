c47db009d2570e086c2dac1e2ffad525904a6790
Igor Leonenko <igrleon@gmail.com>
2016-11-10 16:57:17 +0100

    change protect from forgery

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class BaseController < ApplicationController
-      protect_from_forgery with: :null_session
       rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid
 
       private
