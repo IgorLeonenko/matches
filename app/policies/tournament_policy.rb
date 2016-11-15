03de237de4c436349cfe85c7ca404d3d671e3488
Igor Leonenko <igrleon@gmail.com>
2016-11-15 11:47:00 +0100

    add policy for tournament update/destroy/params

--- app/policies/tournament_policy.rb
+++ app/policies/tournament_policy.rb
@@ -6,7 +6,20 @@ class TournamentPolicy < ApplicationPolicy
     @tournament = tournament
   end
 
-  def update
-    user.creator?(tournament.id)
+  def update?
+    user.creator?(tournament.id) || user.admin?
+  end
+
+  def destroy?
+    user.creator?(tournament.id) || user.admin?
+  end
+
+  def permitted_attributes
+    if tournament.state == 'started'
+      [:title, :description, :picture]
+    else
+      [:title, :game_id, :description, :picture, :start_date,
+       :teams_quantity, :players_in_team, :style, :creator_id]
+    end
   end
 end
