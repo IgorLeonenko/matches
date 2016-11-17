9dafbf646d68020b8ebdc1373a23f32845b7b075
Igor Leonenko <igrleon@gmail.com>
2016-11-17 10:51:36 +0100

    add fields validation to signup form

--- front-client/src/components/SignUp.vue
+++ front-client/src/components/SignUp.vue
@@ -2,32 +2,50 @@
   <div>
     <form id='user'>
       <p>
-        <label for='name'>Name</label>
-        <input type='text' v-model='user.name' name='name'></input>
+        <label for='name' v-bind:class="{'error-label': errors['name']}">Name</label>
+        <input type='text'
+               v-model='user.name' name='name'
+               v-bind:class="{'error-field': errors['name']}">
+        </input>
+        <p v-if="errors['name']" class="error-label">{{errors['name'].toString()}}</p>
       </p>
       <p>
-        <label for='email'>Email</label>
-        <input type='email' v-model='user.email' name='email'></textarea>
+        <label for='email' v-bind:class="{'error-label': errors['email']}">Email</label>
+        <input type='email'
+               v-model='user.email' name='email'
+               v-bind:class="{'error-field': errors['email']}">
+        </input>
+        <p v-if="errors['email']" class="error-label">{{errors['email'].toString()}}</p>
       </p>
       <p>
-        <label for='username'>Username</label>
-        <input type="text" v-model='user.username' name="username"></input>
+        <label for='username' v-bind:class="{'error-label': errors['username']}">Username</label>
+        <input type="text"
+               v-model='user.username' name="username"
+               v-bind:class="{'error-field': errors['username']}">
+        </input>
+        <p v-if="errors['username']" class="error-label">{{errors['username'].toString()}}</p>
       </p>
       <p>
         <label for='avatar'>Avatar</label>
         <input type="text" v-model='user.avatar' name="avatar"></input>
       </p>
       <p>
-        <label for='password'>Password</label>
-        <input type="password" v-model='user.password' name="password"></input>
+        <label for='password' v-bind:class="{'error-label': errors['password']}">Password</label>
+        <input type="password"
+               v-model='user.password' name="password"
+               v-bind:class="{'error-field': errors['password']}">
+        </input>
+        <p v-if="errors['password']" class="error-label">{{errors['password'].toString()}}</p>
       </p>
       <p>
-        <label for='password_confirmation'>Password confirmation</label>
-        <input type="password" v-model='user.password_confirmation'
-               name="password_confirmation">
+        <label for='password_confirmation' v-bind:class="{'error-label': errors['password_confirmation']}">Password confirmation</label>
+        <input type="password"
+               v-model='user.password_confirmation' name="password_confirmation"
+               v-bind:class="{'error-field': errors['password_confirmation']}">
         </input>
+        <p v-if="errors['password_confirmation']" class="error-label">{{errors['password_confirmation'].toString()}}</p>
       </p>
-      <button @click='createUser()'>Sign Up</button>
+      <button type='button' @click='createUser()'>Sign Up</button>
     </form>
   </div>
 </template>
@@ -45,6 +63,11 @@
           password: '',
           password_confirmation: ''
         }
+      }
+    },
+    computed: {
+      errors () {
+        return this.$store.getters.errors
       }
     },
     methods: {
